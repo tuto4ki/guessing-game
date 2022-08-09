3aed82307d9a14ee9a9688e4386e27a27fb7e6b2
Svetlana.Safonova <safonova.sv.a@gmail.com>
2022-08-09 14:35:56 +0300

    feat: change class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+   
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.prev = 0;
+    }
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.prev = Math.round((this.max - this.min) / 2) + this.min;
+        return this.prev;
     }
 
     lower() {
-
+        this.max = this.prev;
     }
 
     greater() {
-
+        this.min = this.prev;
     }
 }
 
